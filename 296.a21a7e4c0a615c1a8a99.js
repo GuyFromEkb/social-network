4030a460baf0138cfb73553e553bcc26c4c90ab1
GuyFromEkb <95606359+GuyFromEkb@users.noreply.github.com>
2023-07-29 13:56:30 +0000

    Deploying to gh-pages from @ GuyFromEkb/social-network@ae93490f7a7a629257d192b2f33a7682c2bc159b 🚀

---
+++ 296.a21a7e4c0a615c1a8a99.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkstudy_project=self.webpackChunkstudy_project||[]).push([[296],{296:(e,s,t)=>{t.r(s),t.d(s,{default:()=>d});var r=t(893),c=t(72);const d=function(){var e=(0,c.$G)("about").t;return(0,r.jsxs)("div",{children:[(0,r.jsx)("div",{children:e("О сайте")}),(0,r.jsx)("div",{children:e("А вот тут мог быть рецепт вкуснейшего рагу")})]})}}}]);
