ab2ff197c1bd6c628c424863b05d5bc4ede2c3ca
GuyFromEkb <guyFromEkb@example.com>
2023-08-05 16:33:34 +0300

    wip ci loki

--- loki.config.js
+++ loki.config.js
@@ -6,14 +6,14 @@ console.log("TARGET1", target)
 module.exports = {
   configurations: {
     "chrome.laptop": {
-      target: "chrome.docker",
+      target: "chrome.app",
       width: 1366,
       height: 768,
       deviceScaleFactor: 1,
       mobile: false,
     },
     "chrome.iphone7": {
-      target: "chrome.docker",
+      target: "chrome.app",
       preset: "iPhone 7",
     },
   },
