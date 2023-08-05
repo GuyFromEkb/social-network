b6cd62cb69017e21c1220b6fb2974a0d213c92a7
GuyFromEkb <guyFromEkb@example.com>
2023-08-05 16:11:02 +0300

    wip ci loki

--- loki.config.js
+++ loki.config.js
@@ -4,15 +4,17 @@ console.log("TARGET", target)
 
 // eslint-disable-next-line
 module.exports = {
-  "chrome.laptop": {
-    target: "chrome.docker",
-    width: 1366,
-    height: 768,
-    deviceScaleFactor: 1,
-    mobile: false,
-  },
-  "chrome.iphone7": {
-    target: "chrome.docker",
-    preset: "iPhone 7",
+  configurations: {
+    "chrome.laptop": {
+      target: "chrome.docker",
+      width: 1366,
+      height: 768,
+      deviceScaleFactor: 1,
+      mobile: false,
+    },
+    "chrome.iphone7": {
+      target: "chrome.docker",
+      preset: "iPhone 7",
+    },
   },
 }
